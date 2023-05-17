0ab2622579ccfbb2c7b50593ff319c60c0ea991f
vishal gupta <vishalguptadishu@gmail.com>
2023-05-17 11:27:27 +0530

    Routing

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './register.css';
+
+const Register = () => {
+  return (
+    <div className="Register">Welcome to Register Page</div>
+  )
+}
+
+export default Register
